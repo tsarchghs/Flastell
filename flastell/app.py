fbbf7d744b760514597f4e587dfb4816a9b383f6
gjergjk71 <gjergjk71@gmail.com>
2018-08-06 23:00:14 +0200

    Creates User table if it doesn't exist

--- flastell/app.py
+++ flastell/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import database as db
+import flask_login
 
+dbPath = "db.sqlite3"
+database = db.Database(dbPath)
+database.connect()
+database.execute("CREATE TABLE IF NOT EXISTS User({});".format(db.getUserSchema()))
+database.close()
 
 app = Flask(__name__)
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 
 if __name__ == "__main__":
