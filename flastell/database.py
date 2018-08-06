8eeff4e7f7cb9f2065f285fd52efaf8278ed62f7
gjergjk71 <gjergjk71@gmail.com>
2018-08-06 22:59:00 +0200

    Created getUserSchema func and execute method

--- flastell/database.py
+++ flastell/database.py
@@ -6,6 +6,17 @@ class Database():
 	def connect(self):
 		self.db = sqlite3.connect(self.dbPath)
 		print("connect")
+	def execute(self,sql):
+		print(sql)
+		c = self.db.cursor()
+		c.execute(sql)
+		self.db.commit()
 	def close(self):
 		self.db.close()
 		print("close")
+
+def getUserSchema():
+	return """id INTEGER PRIMARY KEY,
+			   username TEXT UNIQUE,
+			   password TEXT"""
+
