6a7173edf0322f96858573dcb7af8bf1b1c3badb
gjergjk71 <gjergjk71@gmail.com>
2018-08-14 19:55:47 +0200

    Created Email model

--- flastell/models.py
+++ flastell/models.py
@@ -10,4 +10,15 @@ class User(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
 	username = db.Column(db.String)
 	password = db.Column(db.String)
+	def __repr__(self):
+		return "<User {}>".format(username)
+
+class Email(db.Model):
+	id = db.Column(db.Integer,primary_key=True)
+	sender_id = db.Column(db.Integer,db.ForeignKey("user.id"))
+	receiver_id = db.Column(db.Integer,db.ForeignKey("user.id"))
+	title = db.Column(db.String(500))
+	description = db.Column(db.String)
+	def __repr__(self):
+		return "<Email {}>".format(id)
 
